efd2975e97840ccfc943eae6075797312ddda30a
sukumar001reddyp-creator <sukumar001reddyp@gmail.com>
2026-06-03 18:52:02 +0530

    Added login and register system
    Added user registration and login functionality with SQLite database integration.

--- app.py
+++ app.py
@@ -1,10 +1,104 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "jobfinder_secret"
+
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT UNIQUE,
+    password TEXT,
+    jobrole TEXT,
+    location TEXT,
+    experience TEXT
+)
+""")
+
+conn.commit()
+conn.close()
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+
+        if password != confirm_password:
+            return "Passwords do not match"
+
+        jobrole = request.form["jobrole"]
+        location = request.form["location"]
+        experience = request.form["experience"]
+
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(
+                "INSERT INTO users (name,email,password,jobrole,location,experience) VALUES (?,?,?,?,?,?)",
+                (name, email, password, jobrole, location, experience)
+            )
+            conn.commit()
+
+        except:
+            conn.close()
+            return "Email already exists"
+
+        conn.close()
+        return redirect("/login")
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE email=? AND password=?",
+            (email, password)
+        )
+
+        user = cursor.fetchone()
+
+        conn.close()
+
+        if user:
+            session["user"] = email
+            return redirect("/")
+        else:
+            return "Invalid Login"
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect("/")
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
