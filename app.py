8bf98a642b86136f88906aebb2c03edbb7056b76
sukumar001reddyp-creator <sukumar001reddyp@gmail.com>
2026-06-08 19:18:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'jobfinder_secret_strong
 
 # Flask-Mail Configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'jobfinderpro85@gmail.com'
 app.config['MAIL_PASSWORD'] = 'eole ihak wvty vumv'
 
